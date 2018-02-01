c34e5065d997b93cfe455b5aad408dd8d33a5a35
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-02-01 14:32:29 +0700

    added test cases for executeTemplate

--- test/documentActions/mailMerge/executeTemplateTests.ts
+++ test/documentActions/mailMerge/executeTemplateTests.ts
@@ -53,16 +53,16 @@ describe("executeTemplate", () => {
         });
     });
 
-    describe("postDocumentExecuteMailMerge function", () => {
-        describe("simple mailMerge", () => {
+    describe("postExecuteTemplate function", () => {
+        describe("without regions", () => {
             it("should return response with code 200", () => {
 
                 const storageApi = BaseTest.initializeStorageApi();
                 const wordsApi = BaseTest.initializeWordsApi();
 
-                const templateLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/SampleExecuteTemplate.docx";
-                const dataLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/SampleExecuteTemplateData.txt";
-                const remoteFileName = "TestSimpleExecuteTemplate.docx";
+                const templateLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/ExecuteTemplateWithoutRegions.doc";
+                const dataLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/ExecuteTemplateWithoutRegions.json";
+                const remoteFileName = "ExecuteTemplateWithoutRegions.doc";
                 const remotePath = BaseTest.remoteBaseTestDataFolder + testFolder;
 
                 return new Promise((resolve) => {
@@ -89,7 +89,42 @@ describe("executeTemplate", () => {
             });
         });
 
-        describe("executeTemplate with images", () => {
+        describe("with regions", () => {
+            it("should return response with code 200", () => {
+
+                const storageApi = BaseTest.initializeStorageApi();
+                const wordsApi = BaseTest.initializeWordsApi();
+
+                const templateLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/SampleExecuteTemplate.docx";
+                const dataLocalPath = BaseTest.localBaseTestDataFolder + testFolder + "/SampleExecuteTemplateData.txt";
+                const remoteFileName = "ExecuteTemplateWithRegions.docx";
+                const remotePath = BaseTest.remoteBaseTestDataFolder + testFolder;
+
+                return new Promise((resolve) => {
+                    storageApi.PutCreate(remotePath + "/" + remoteFileName, null, null, templateLocalPath, (responseMessage) => {
+                        expect(responseMessage.status).to.equal("OK");
+                        resolve();
+                    });
+                })
+                    .then(() => {
+                        const request = new PostExecuteTemplateRequest();
+                        request.name = remoteFileName;
+                        request.folder = remotePath;
+                        request.data = fs.readFileSync(dataLocalPath, "utf8");
+                        request.withRegions = true;
+
+                        // Act
+                        return wordsApi.postExecuteTemplate(request)
+                            .then((result) => {
+                                // Assert
+                                expect(result.body.code).to.equal(200);
+                                expect(result.response.statusCode).to.equal(200);
+                            });
+                    });
+            });
+        });
+
+        describe("with images", () => {
             it("should return response with code 200", () => {
 
                 const storageApi = BaseTest.initializeStorageApi();

7c765c13e43cc0688fb182c621bcd5c71744b6ee
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-04-11 08:56:25 +0700

    fix build

--- scripts/runTests.bat
+++ scripts/runTests.bat
@@ -1,2 +1,10 @@
-call npm.cmd install
-call npm.cmd run test-jenkins
+call npm.cmd install ||  goto :error
+call npm.cmd run gulp build ||  goto :error
+call npm.cmd run lint ||  goto :error
+call npm.cmd run test-jenkins ||  goto :error
+
+goto :EOF
+
+:error
+echo Failed with error #%errorlevel%.
+exit /b %errorlevel%

fdf929462e2ccd94f43c1ba09746676432975d43
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-04-26 18:14:01 +0700

    cosmetic

--- test/documentActions/classificationTests.ts
+++ test/documentActions/classificationTests.ts
@@ -25,7 +25,7 @@
 import { expect } from "chai";
 import "mocha";
 
-import { ClassifyDocumentRequest, ClassificationRequestParameters, ClassifyRequest } from "../../src/model/model";
+import { ClassificationRequestParameters, ClassifyDocumentRequest, ClassifyRequest } from "../../src/model/model";
 import * as BaseTest from "../baseTest";
 
 const testFolder = "Common";

5d69e6594deebc3d410b729f5c2101828256d96f
Evgeny Ivanov <Evgeny.Ivanov@aspose.com>
2018-06-27 13:29:25 +0700

    added release notes and updated version

--- src/internal/requestHelper.ts
+++ src/internal/requestHelper.ts
@@ -87,7 +87,7 @@ async function invokeApiMethodInternal(requestOptions: request.Options, confgura
     }
 
     requestOptions.headers["x-aspose-client"] = "nodejs sdk";
-    requestOptions.headers["x-aspose-client-version"] = "18.5";
+    requestOptions.headers["x-aspose-client-version"] = "18.6";
 
     const auth = confguration.authentication;
     if (!notApplyAuthToRequest) {

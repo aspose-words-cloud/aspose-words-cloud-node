b81d2e3ab61bd6e5de38eb925631c17f81211740
evgeny.ivanov <evgeny.ivanov>
2020-12-27 22:42:20 -0600

    SDK regenerated by CI server [ci skip]

--- src/internal/requestHelper.ts
+++ src/internal/requestHelper.ts
@@ -90,7 +90,7 @@ async function invokeApiMethodInternal(requestOptions: request.OptionsWithUri, c
     }
 
     requestOptions.headers["x-aspose-client"] = "nodejs sdk";
-    requestOptions.headers["x-aspose-client-version"] = "20.11";
+    requestOptions.headers["x-aspose-client-version"] = "20.12";
 
 	requestOptions.uri = encodeURI(requestOptions.uri.toString());
 

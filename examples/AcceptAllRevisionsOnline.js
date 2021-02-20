b5f1f4e23506bc4778ec94b31f940253234e75e1
ivan.kishchenko <ivan.kishchenko@aspose.com>
2021-02-20 15:48:41 +0300

    Codegen

---
+++ examples/AcceptAllRevisionsOnline.js
@@ -0,0 +1,15 @@
+const clientId = "####-####-####-####-####";
+const secret = "##################";
+const wordsApi = new WordsApi(clientId, secret);
+const documentsDir = "./";
+const fileName  = "test_doc.docx";
+
+// Calls AcceptAllRevisionsOnline method for document in cloud.
+const request = new model.AcceptAllRevisionsOnlineRequest({
+    document: fs.createReadStream(documentsDir + fileName)
+});
+
+return wordsApi.acceptAllRevisionsOnline(request)
+.then((requestResult) => {
+    fs.writeFileSync("test_result.docx", requestResult.document);
+});

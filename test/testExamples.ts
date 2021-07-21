56c1bc9f80e7b7bcd6c04c07db027e2375bb945d
evgeny.ivanov <evgeny.ivanov>
2021-07-21 07:35:36 -0500

    SDK regenerated by CI server [ci skip]

--- test/testExamples.ts
+++ test/testExamples.ts
@@ -95,27 +95,4 @@ describe("examples", () => {
             });
         });
     });
-
-    describe("exampleUpdateBookmark", () => {
-        it("should return response with code 200", () => {
-            const wordsApi = BaseTest.initializeWordsApi();
-            const remoteFileName = "Sample.docx";
-            const bookmarkName = "aspose";
-
-            const updateBookmark = new model.UpdateBookmarkRequest({
-                name: remoteFileName,
-                bookmarkName: bookmarkName,
-                bookmarkData: new model.BookmarkData({
-                    name: bookmarkName,
-                    text: "New Bookmark Text"
-                })
-            });
-
-            return wordsApi.updateBookmark(updateBookmark)
-            .then((updateBookmarkResult) => {
-                // tslint:disable-next-line:no-console
-                console.log("Result of UpdateBookmark: ", updateBookmarkResult);
-            });
-        });
-    });
 });

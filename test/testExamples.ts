bec8f0d44723c854604a606b5cdac3dc405ef4dd
Dmitry Finozhenok <dmitry.finozhenok@aspose.com>
2021-09-30 18:28:43 +0900

    Test pass encypting

--- test/testExamples.ts
+++ test/testExamples.ts
@@ -40,8 +40,8 @@ describe("examples", () => {
         return wordsApi.uploadFileToStorage(
             'test_doc.docx',
             './ExamplesData/test_doc.docx'
-        ).then((result944546255) => {
-            expect(result944546255.response.statusMessage).to.equal("OK");
+        ).then((result1037131313) => {
+            expect(result1037131313.response.statusMessage).to.equal("OK");
         });
 
     });

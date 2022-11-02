6a66dbb4bd54184f65cb6f8b4c9c9b3fb8beaa13
ivan.kishchenko <ivan.kishchenko@aspose.com>
2022-11-02 17:30:41 +0400

    Fix BDD tests

--- bdd/steps/common/storageSteps.ts
+++ bdd/steps/common/storageSteps.ts
@@ -46,7 +46,10 @@ Given(/^There is no file (.*) on storage in (.*) folder$/, {timeout: 60000}, asy
     const request = new DeleteFileRequest();
     request.path = remotePath;
 
-    await this.wordsApi.deleteFile(request);
+    try {
+        await this.wordsApi.deleteFile(request);
+    }
+    catch {}
 });
 
 Then(/^document (.*) is existed on storage in (.*) folder$/, {timeout: 60000}, async function (documentName, folder) {

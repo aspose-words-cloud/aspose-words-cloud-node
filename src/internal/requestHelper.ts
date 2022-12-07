179c31bbe9c6f6551c62420050df3f5ab4f68e63
jenkins <jenkins.aspose@gmail.com>
2022-12-06 22:25:25 -0600

    SDK regenerated by CI server [ci skip]

--- src/internal/requestHelper.ts
+++ src/internal/requestHelper.ts
@@ -131,7 +131,7 @@ async function invokeApiMethodInternal(requestOptions: request.OptionsWithUri, c
     requestOptions.timeout = 1000 * confguration.timeout;
 
     requestOptions.headers["x-aspose-client"] = "nodejs sdk";
-    requestOptions.headers["x-aspose-client-version"] = "22.11";
+    requestOptions.headers["x-aspose-client-version"] = "22.12";
 
 	requestOptions.uri = encodeURI(requestOptions.uri.toString());
 

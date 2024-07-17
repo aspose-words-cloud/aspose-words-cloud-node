ea6e388ea424ea22160ca60e8b419d35eeded291
jenkins <jenkins.aspose@gmail.com>
2024-07-17 09:48:04 -0500

    SDK regenerated by CI server [ci skip]

--- src/model/pdfSaveOptionsData.ts
+++ src/model/pdfSaveOptionsData.ts
@@ -412,7 +412,9 @@ export namespace PdfSaveOptionsData {
         PdfA2a = 'PdfA2a' as any,
         PdfA2u = 'PdfA2u' as any,
         PdfA4 = 'PdfA4' as any,
-        PdfUa1 = 'PdfUa1' as any
+        PdfA4Ua2 = 'PdfA4Ua2' as any,
+        PdfUa1 = 'PdfUa1' as any,
+        PdfUa2 = 'PdfUa2' as any
     }
 
     export enum CustomPropertiesExportEnum {

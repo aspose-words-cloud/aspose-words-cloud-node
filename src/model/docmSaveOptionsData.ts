767b5e790cd7765bdb54a5e8ea0f0fafdc90c4ac
jenkins <jenkins.aspose@gmail.com>
2026-01-16 09:54:54 -0600

    SDK regenerated by CI server [ci skip]

--- src/model/docmSaveOptionsData.ts
+++ src/model/docmSaveOptionsData.ts
@@ -1,7 +1,7 @@
 /*
  * --------------------------------------------------------------------------------
  * <copyright company="Aspose" file="docmSaveOptionsData.ts">
- *   Copyright (c) 2025 Aspose.Words for Cloud
+ *   Copyright (c) 2026 Aspose.Words for Cloud
  * </copyright>
  * <summary>
  *   Permission is hereby granted, free of charge, to any person obtaining a copy
